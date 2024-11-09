eed313689da6a3056a17739c9454c0a67d814b34
NgNhatThanh <thanhtobot159@gmail.com>
2024-11-09 22:03:57 +0700

    add mock data

---
+++ mock-data/tags.sql
@@ -0,0 +1,4 @@
+INSERT INTO social_network.tags (name) VALUES ('Java');
+INSERT INTO social_network.tags (name) VALUES ('React');
+INSERT INTO social_network.tags (name) VALUES ('Back-End');
+INSERT INTO social_network.tags (name) VALUES ('Front-End');
