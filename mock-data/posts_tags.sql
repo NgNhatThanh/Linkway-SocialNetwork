ad61dc99ea5296a3fb79ab2f7a693427bf8b6d92
NgNhatThanh <thanhtobot159@gmail.com>
2024-11-27 00:18:44 +0700

    doc: update README

--- mock-data/posts_tags.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (1, 1);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (2, 1);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (1, 2);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (3, 2);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (2, 3);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (3, 3);
-INSERT INTO social_network.posts_tags (post_id, tag_id) VALUES (3, 4);
--- mock-data/users_tags.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (1, 1);
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (2, 3);
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (3, 1);
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (3, 3);
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (2, 1);
-INSERT INTO social_network.users_tags (tag_id, user_id) VALUES (1, 3);
